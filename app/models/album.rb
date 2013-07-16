9ca50ad4d3f231b54328c3c3f74caf0940d37710
Amy Erz <wingmerz@gmail.com>
2013-07-15 21:04:50 -0700

    add models relationships

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,5 @@
 class Album < ActiveRecord::Base
   has_many :photos
   attr_accessible :name
+  has_many :comments, as: :commentable
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,5 @@ class Photo < ActiveRecord::Base
   belongs_to :album
   attr_accessible :album_id, :mvimage, :remote_image_url
   mount_uploader :mvimage, ImageUploader
+  has_many :comments, as: :commentable
 end
